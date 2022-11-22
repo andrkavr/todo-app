e1deda632544289b7689f5feb6f6f8036896ec07
andrkavr <72137459+andrkavr@users.noreply.github.com>
2022-11-22 14:14:52 +0100

    added functions for getting, creating, updating and deleting todo-items.

---
+++ src/models/todo.ts
@@ -0,0 +1,24 @@
+import { ITodo } from "../types/todo";
+import { model, Schema } from "mongoose";
+
+const todoSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<ITodo>("Todo", todoSchema);
